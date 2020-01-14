b6bbbd8bfa9a59b4d0bda6a531c5650b25347d23
Gaston Donnet <Gassti7@gmail.com>
2020-01-14 14:54:23 -0300

    Update app.711639f2.js

--- public/js/app.711639f2.js
+++ public/js/app.711639f2.js
@@ -1,2 +1,131 @@
-(function(e){function t(t){for(var r,u,l=t[0],c=t[1],s=t[2],i=0,p=[];i<l.length;i++)u=l[i],Object.prototype.hasOwnProperty.call(a,u)&&a[u]&&p.push(a[u][0]),a[u]=0;for(r in c)Object.prototype.hasOwnProperty.call(c,r)&&(e[r]=c[r]);f&&f(t);while(p.length)p.shift()();return o.push.apply(o,s||[]),n()}function n(){for(var e,t=0;t<o.length;t++){for(var n=o[t],r=!0,u=1;u<n.length;u++){var c=n[u];0!==a[c]&&(r=!1)}r&&(o.splice(t--,1),e=l(l.s=n[0]))}return e}var r={},a={app:0},o=[];function u(e){return l.p+"js/"+({}[e]||e)+"."+{"chunk-2d217e3e":"66e5e06a"}[e]+".js"}function l(t){if(r[t])return r[t].exports;var n=r[t]={i:t,l:!1,exports:{}};return e[t].call(n.exports,n,n.exports,l),n.l=!0,n.exports}l.e=function(e){var t=[],n=a[e];if(0!==n)if(n)t.push(n[2]);else{var r=new Promise((function(t,r){n=a[e]=[t,r]}));t.push(n[2]=r);var o,c=document.createElement("script");c.charset="utf-8",c.timeout=120,l.nc&&c.setAttribute("nonce",l.nc),c.src=u(e);var s=new Error;o=function(t){c.onerror=c.onload=null,clearTimeout(i);var n=a[e];if(0!==n){if(n){var r=t&&("load"===t.type?"missing":t.type),o=t&&t.target&&t.target.src;s.message="Loading chunk "+e+" failed.\n("+r+": "+o+")",s.name="ChunkLoadError",s.type=r,s.request=o,n[1](s)}a[e]=void 0}};var i=setTimeout((function(){o({type:"timeout",target:c})}),12e4);c.onerror=c.onload=o,document.head.appendChild(c)}return Promise.all(t)},l.m=e,l.c=r,l.d=function(e,t,n){l.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},l.r=function(e){"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},l.t=function(e,t){if(1&t&&(e=l(e)),8&t)return e;if(4&t&&"object"===typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(l.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)l.d(n,r,function(t){return e[t]}.bind(null,r));return n},l.n=function(e){var t=e&&e.__esModule?function(){return e["default"]}:function(){return e};return l.d(t,"a",t),t},l.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},l.p="/",l.oe=function(e){throw console.error(e),e};var c=window["webpackJsonp"]=window["webpackJsonp"]||[],s=c.push.bind(c);c.push=t,c=c.slice();for(var i=0;i<c.length;i++)t(c[i]);var f=s;o.push([0,"chunk-vendors"]),n()})({0:function(e,t,n){e.exports=n("56d7")},"56d7":function(e,t,n){"use strict";n.r(t);n("e260"),n("e6cf"),n("cca6"),n("a79d");var r=n("2b0e"),a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{attrs:{id:"app"}},[n("Navbar"),n("router-view")],1)},o=[],u=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("b-navbar",{attrs:{toggleable:"lg",type:"dark",variant:"dark"}},[n("b-navbar-brand",{attrs:{to:"/"}},[e._v("Blog de Notas")]),n("b-navbar-toggle",{attrs:{target:"nav-collapse"}}),n("b-collapse",{attrs:{id:"nav-collapse","is-nav":""}},[n("b-navbar-nav",{staticClass:"ml-auto"},[n("b-nav-item",{attrs:{to:{name:"home"},"active-class":"active",exact:""}},[e._v("Home")]),n("b-nav-item",{attrs:{to:{name:"notas"},"active-class":"active",exact:""}},[e._v("Notas")])],1)],1)],1)],1)},l=[],c={},s=c,i=n("2877"),f=Object(i["a"])(s,u,l,!1,null,null,null),p=f.exports,d={components:{Navbar:p}},v=d,m=Object(i["a"])(v,a,o,!1,null,null,null),b=m.exports,h=(n("d3b7"),n("8c4f")),g=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},y=[function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("div",{staticClass:"home"},[r("img",{attrs:{alt:"Vue logo",src:n("cf05")}})])}],_={name:"home",components:{}},w=_,j=Object(i["a"])(w,g,y,!1,null,null,null),O=j.exports;r["default"].use(h["a"]);var x=[{path:"/",name:"home",component:O},{path:"/notas",name:"notas",component:function(){return n.e("chunk-2d217e3e").then(n.bind(null,"c93e"))}}],k=new h["a"]({mode:"history",base:"/",routes:x}),P=k,E=n("2f62");r["default"].use(E["a"]);var S=new E["a"].Store({state:{},mutations:{},actions:{},modules:{}}),T=n("5f5b"),$=(n("f9e3"),n("2dd8"),n("bc3a")),C=n.n($),M=n("a7fe"),N=n.n(M);r["default"].use(T["a"]),r["default"].use(N.a,C.a),C.a.defaults.baseURL="http://localhost:3000/api",r["default"].config.productionTip=!1,new r["default"]({router:P,store:S,render:function(e){return e(b)}}).$mount("#app")},cf05:function(e,t,n){e.exports=n.p+"img/logo.82b9c7a5.png"}});
+(function(e) {
+    function t(t) {
+        for (var r, u, l = t[0], c = t[1], s = t[2], i = 0, p = []; i < l.length; i++) u = l[i], Object.prototype.hasOwnProperty.call(a, u) && a[u] && p.push(a[u][0]), a[u] = 0;
+        for (r in c) Object.prototype.hasOwnProperty.call(c, r) && (e[r] = c[r]);
+        f && f(t);
+        while (p.length) p.shift()();
+        return o.push.apply(o, s || []), n()
+    }
+
+    function n() {
+        for (var e, t = 0; t < o.length; t++) {
+            for (var n = o[t], r = !0, u = 1; u < n.length; u++) {
+                var c = n[u];
+                0 !== a[c] && (r = !1)
+            }
+            r && (o.splice(t--, 1), e = l(l.s = n[0]))
+        }
+        return e
+    }
+    var r = {},
+        a = { app: 0 },
+        o = [];
+
+    function u(e) { return l.p + "js/" + ({}[e] || e) + "." + { "chunk-2d217e3e": "66e5e06a" }[e] + ".js" }
+
+    function l(t) { if (r[t]) return r[t].exports; var n = r[t] = { i: t, l: !1, exports: {} }; return e[t].call(n.exports, n, n.exports, l), n.l = !0, n.exports }
+    l.e = function(e) {
+        var t = [],
+            n = a[e];
+        if (0 !== n)
+            if (n) t.push(n[2]);
+            else {
+                var r = new Promise((function(t, r) { n = a[e] = [t, r] }));
+                t.push(n[2] = r);
+                var o, c = document.createElement("script");
+                c.charset = "utf-8", c.timeout = 120, l.nc && c.setAttribute("nonce", l.nc), c.src = u(e);
+                var s = new Error;
+                o = function(t) {
+                    c.onerror = c.onload = null, clearTimeout(i);
+                    var n = a[e];
+                    if (0 !== n) {
+                        if (n) {
+                            var r = t && ("load" === t.type ? "missing" : t.type),
+                                o = t && t.target && t.target.src;
+                            s.message = "Loading chunk " + e + " failed.\n(" + r + ": " + o + ")", s.name = "ChunkLoadError", s.type = r, s.request = o, n[1](s)
+                        }
+                        a[e] = void 0
+                    }
+                };
+                var i = setTimeout((function() { o({ type: "timeout", target: c }) }), 12e4);
+                c.onerror = c.onload = o, document.head.appendChild(c)
+            }
+        return Promise.all(t)
+    }, l.m = e, l.c = r, l.d = function(e, t, n) { l.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: n }) }, l.r = function(e) { "undefined" !== typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }), Object.defineProperty(e, "__esModule", { value: !0 }) }, l.t = function(e, t) {
+        if (1 & t && (e = l(e)), 8 & t) return e;
+        if (4 & t && "object" === typeof e && e && e.__esModule) return e;
+        var n = Object.create(null);
+        if (l.r(n), Object.defineProperty(n, "default", { enumerable: !0, value: e }), 2 & t && "string" != typeof e)
+            for (var r in e) l.d(n, r, function(t) { return e[t] }.bind(null, r));
+        return n
+    }, l.n = function(e) { var t = e && e.__esModule ? function() { return e["default"] } : function() { return e }; return l.d(t, "a", t), t }, l.o = function(e, t) { return Object.prototype.hasOwnProperty.call(e, t) }, l.p = "/", l.oe = function(e) { throw console.error(e), e };
+    var c = window["webpackJsonp"] = window["webpackJsonp"] || [],
+        s = c.push.bind(c);
+    c.push = t, c = c.slice();
+    for (var i = 0; i < c.length; i++) t(c[i]);
+    var f = s;
+    o.push([0, "chunk-vendors"]), n()
+})({
+    0: function(e, t, n) { e.exports = n("56d7") },
+    "56d7": function(e, t, n) {
+        "use strict";
+        n.r(t);
+        n("e260"), n("e6cf"), n("cca6"), n("a79d");
+        var r = n("2b0e"),
+            a = function() {
+                var e = this,
+                    t = e.$createElement,
+                    n = e._self._c || t;
+                return n("div", { attrs: { id: "app" } }, [n("Navbar"), n("router-view")], 1)
+            },
+            o = [],
+            u = function() {
+                var e = this,
+                    t = e.$createElement,
+                    n = e._self._c || t;
+                return n("div", [n("b-navbar", { attrs: { toggleable: "lg", type: "dark", variant: "dark" } }, [n("b-navbar-brand", { attrs: { to: "/" } }, [e._v("Blog de Notas")]), n("b-navbar-toggle", { attrs: { target: "nav-collapse" } }), n("b-collapse", { attrs: { id: "nav-collapse", "is-nav": "" } }, [n("b-navbar-nav", { staticClass: "ml-auto" }, [n("b-nav-item", { attrs: { to: { name: "home" }, "active-class": "active", exact: "" } }, [e._v("Home")]), n("b-nav-item", { attrs: { to: { name: "notas" }, "active-class": "active", exact: "" } }, [e._v("Notas")])], 1)], 1)], 1)], 1)
+            },
+            l = [],
+            c = {},
+            s = c,
+            i = n("2877"),
+            f = Object(i["a"])(s, u, l, !1, null, null, null),
+            p = f.exports,
+            d = { components: { Navbar: p } },
+            v = d,
+            m = Object(i["a"])(v, a, o, !1, null, null, null),
+            b = m.exports,
+            h = (n("d3b7"), n("8c4f")),
+            g = function() {
+                var e = this,
+                    t = e.$createElement;
+                e._self._c;
+                return e._m(0)
+            },
+            y = [function() {
+                var e = this,
+                    t = e.$createElement,
+                    r = e._self._c || t;
+                return r("div", { staticClass: "home" }, [r("img", { attrs: { alt: "Vue logo", src: n("cf05") } })])
+            }],
+            _ = { name: "home", components: {} },
+            w = _,
+            j = Object(i["a"])(w, g, y, !1, null, null, null),
+            O = j.exports;
+        r["default"].use(h["a"]);
+        var x = [{ path: "/", name: "home", component: O }, { path: "/notas", name: "notas", component: function() { return n.e("chunk-2d217e3e").then(n.bind(null, "c93e")) } }],
+            k = new h["a"]({ mode: "history", base: "/", routes: x }),
+            P = k,
+            E = n("2f62");
+        r["default"].use(E["a"]);
+        var S = new E["a"].Store({ state: {}, mutations: {}, actions: {}, modules: {} }),
+            T = n("5f5b"),
+            $ = (n("f9e3"), n("2dd8"), n("bc3a")),
+            C = n.n($),
+            M = n("a7fe"),
+            N = n.n(M);
+        r["default"].use(T["a"]), r["default"].use(N.a, C.a), C.a.defaults.baseURL = "https://dashboard.heroku.com/apps/app-curso-vue-1/api", r["default"].config.productionTip = !1, new r["default"]({ router: P, store: S, render: function(e) { return e(b) } }).$mount("#app")
+    },
+    cf05: function(e, t, n) { e.exports = n.p + "img/logo.82b9c7a5.png" }
+});
 //# sourceMappingURL=app.711639f2.js.map
